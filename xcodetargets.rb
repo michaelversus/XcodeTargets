66a30b9b4d5f904c0f91eaa5f515e90c0c2255d8
Michalis Karagiorgos <m.karagiorgos@kaizengaming.com>
2025-12-29 17:54:16 +0200

    bump up version

--- xcodetargets.rb
+++ xcodetargets.rb
@@ -1,8 +1,8 @@
 class Xcodetargets < Formula
   desc "Swift implicit import checks for SPM packages and Xcodeproj"
   homepage "https://github.com/michaelversus/XcodeTargets"
-  url "https://github.com/michaelversus/XcodeTargets.git", tag: "0.1.6"
-  version "0.1.6"
+  url "https://github.com/michaelversus/XcodeTargets.git", tag: "0.1.7"
+  version "0.1.7"
 
   depends_on "xcode": [:build]
 
